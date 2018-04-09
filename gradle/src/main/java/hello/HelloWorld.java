fb117deba084ca4c073649c9147d88565ad59de6
Ana Cunha <mcunha.ana@gmail.com>
2018-04-09 18:10:17 -0300

    Initial commit from "Building Java Projects with Gradle"

---
+++ gradle/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        final Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
