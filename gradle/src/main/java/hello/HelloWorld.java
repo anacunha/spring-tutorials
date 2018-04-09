9ebeeffc577ff8bcc1e9814822be5915e31ab0ec
Ana Cunha <mcunha.ana@gmail.com>
2018-04-09 18:21:03 -0300

    Adds dependencies to "Building Java Projects with Gradle"

--- gradle/src/main/java/hello/HelloWorld.java
+++ gradle/src/main/java/hello/HelloWorld.java
@@ -1,8 +1,13 @@
 package hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        final LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
 
         final Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
