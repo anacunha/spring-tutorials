d7efbc920815957f1d2924d3fa01773253cc7494
Ana Cunha <mcunha.ana@gmail.com>
2018-04-16 21:07:09 -0300

    Makes "Consuming a RESTful Web Service" executable

---
+++ consuming-rest/src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+
+        final RestTemplate restTemplate = new RestTemplate();
+        final Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        
+        log.info(quote.toString());
+    }
+}
