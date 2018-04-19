d07ef593d2905732191064c4fdd3d5d323397d36
Ana Cunha <mcunha.ana@gmail.com>
2018-04-18 21:45:30 -0300

    Managing the "Consuming a RESTful Web Service" Application Lifecycle with Spring Boot

--- consuming-rest/src/main/java/hello/Application.java
+++ consuming-rest/src/main/java/hello/Application.java
@@ -2,19 +2,33 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@SpringBootApplication
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
 
-        final RestTemplate restTemplate = new RestTemplate();
-        final Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        
-        log.info(quote.toString());
+    @Bean
+    public RestTemplate restTemplate(final RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(final RestTemplate restTemplate) throws Exception {
+
+        return args -> {
+            final Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 }
